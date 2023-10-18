861f6a64dcd9bfa1a875f0f2f7844ea0619eff27
Cristian Quilumbaquin <crysn9dav@gmail.com>
2023-10-18 10:12:31 -0500

    Agregar archivos de proyecto.

---
+++ Hospital/Models/Paciente.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Hospital.Models
+{
+    public partial class Paciente
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; } = null!;
+        public string Apellidos { get; set; } = null!;
+        public string Direccion { get; set; } = null!;
+        public string Telefono { get; set; } = null!;
+    }
+}
